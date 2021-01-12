2bc234e7320dce0b5958cd5ca31b390862337bbe
minmeeen <sirapat.mp@gmail.com>
2021-01-12 15:40:22 +0700

    lab10_1edit

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -1,2 +1,22 @@
-// cout << "Press Enter 3 times to reveal your future.";
-// cout << "You will get A in this 261102.";
+#include <iostream>
+#include <string>
+#include <ctime>
+using namespace std;
+
+int main(){
+    string x[9] = {"A", "B+", "B", "C+", "C", "D+", "D", "F", "W"};
+    srand(time(0));
+    int grade;
+
+    cout << "Press Enter 3 times to reveal your future.";
+    cin.get();
+    cin.get();
+    cin.get();
+    grade = rand()%9;
+
+    cout << "You will get "<< x[grade] <<" in this 261102.";
+    
+    return 0;
+}
+
+
